d6abb6504f2e41938cc8890fc095e4eed56765e4
Imam Ramadhan <imamrmdn99@gmail.com>
2022-12-02 18:37:27 +0700

    update : picture

--- app/src/main/java/com/example/myfirst/MainActivity.kt
+++ app/src/main/java/com/example/myfirst/MainActivity.kt
@@ -3,23 +3,35 @@ package com.example.myfirst
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.myfirst.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
 
-        btn1.setOnClickListener {
-            val name = nameText.text.toString()
+        binding.btn1.setOnClickListener {
+            val name = binding.nameText.text.toString()
             if (name.trim() == "")
                 Toast.makeText(
                     this@MainActivity,
                     "nama tidak boleh kosong",
                     Toast.LENGTH_SHORT
                 ).show()
-            else
-                textBase.text = name
+            else {
+                binding.textBase.text = name
+                val imgRes = resources.getIdentifier(name, "drawable", packageName)
+                binding.img1.setImageResource(imgRes)
+            }
         }
     }
 }
